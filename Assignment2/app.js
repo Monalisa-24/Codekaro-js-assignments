7837eab401d2b80da8b3d61e396134e664bf7f26
Monalisa Saha <116990954+Monalisa-24@users.noreply.github.com>
2023-12-27 13:44:19 +0530

    Add files via upload

---
+++ Assignment2/app.js
@@ -0,0 +1,20 @@
+// create an empty object called "Person"
+let person = {};
+
+// add the following property 
+person.name = "John";
+person.age = 30;
+person.city = "New York";
+
+console.log(person);
+
+// remove the "age" property
+delete person.age;
+
+// add a new property called "job" with the value "Engineer"
+person.job = "Engineer";
+
+// update the "city" property to "San Francisco"
+person.city = " San Fransisco"; 
+
+console.log(person);
